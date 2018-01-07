a6ba668b33cb604451c5d686968c11347f9f090b
Kat Reinhart <kat.reinhart@gmail.com>
2018-01-07 14:44:21 -0800

    Complete REST routes

---
+++ README.md
@@ -0,0 +1,3 @@
+## Basic REST API built in Go
+From [this Medium article](https://medium.com/@thedevsaddam/build-restful-api-service-in-golang-using-gin-gonic-framework-85b1a6e176f3) but using Postgres instead of MySQL
+
--- main.go
+++ main.go
@@ -11,8 +11,8 @@ import (
 
 var db *gorm.DB
 
+// init open a db connection
 func init() {
-	// open a db connection
 	var err error
 	db, err = gorm.Open("postgres", "host=localhost user=gorm dbname=todo_dev sslmode=disable password=gorm")
 	if err != nil {
@@ -45,11 +45,14 @@ func main() {
 		v1.POST("/", createTodo)
 		v1.GET("/", fetchAllTodo)
 		v1.GET("/:id", fetchSingleTodo)
+		v1.PUT("/:id", updateTodo)
+		v1.DELETE("/:id", deleteTodo)
 	}
 
 	router.Run()
 }
 
+// createTodo create a todo
 func createTodo(c *gin.Context) {
 	completed, _ := strconv.Atoi(c.PostForm("completed"))
 	todo := todoModel{Title: c.PostForm("title"), Completed: completed}
@@ -104,4 +107,37 @@ func fetchSingleTodo(c *gin.Context) {
 	_todo := transformedTodo{ID: todo.ID, Title: todo.Title, Completed: completed}
 
 	c.JSON(http.StatusOK, gin.H{"status": http.StatusOK, "data": _todo})
+}
+
+// updateTodo update a todo
+func updateTodo(c *gin.Context) {
+	var todo todoModel
+	todoID := c.Param("id")
+
+	db.First(&todo, todoID)
+
+	if todo.ID == 0 {
+		c.JSON(http.StatusNotFound, gin.H{"status": http.StatusNotFound, "message": "No todo found!"})
+		return
+	}
+
+	db.Model(&todo).Update("title", c.PostForm("title"))
+	completed, _ := strconv.Atoi(c.PostForm("completed"))
+	db.Model(&todo).Update("completed", completed)
+	c.JSON(http.StatusOK, gin.H{"status": http.StatusOK, "message": "Todo updated successfully!"})
+}
+
+// deleteTodo remove a todo
+func deleteTodo(c *gin.Context) {
+	var todo todoModel
+	todoID := c.Param("id")
+
+	db.First(&todo, todoID)
+	if todo.ID == 0 {
+		c.JSON(http.StatusNotFound, gin.H{"status": http.StatusNotFound, "message": "No todo found!"})
+		return
+	}
+
+	db.Delete(&todo)
+	c.JSON(http.StatusOK, gin.H{"status": http.StatusOK, "message": "Todo deleted successfully!"})
 }
